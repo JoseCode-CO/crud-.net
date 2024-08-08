83f56ffcbbef06e9ddab6e206d2f4a715c6c83aa
CODE WITH JOSE DAVID <48775594+JoseCode-CO@users.noreply.github.com>
2024-08-07 20:57:54 -0500

    Add entity framework

---
+++ Context/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Context
+{
+    public class AppDBContext: DbContext 
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
