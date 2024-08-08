68ce6133b0c851ed838cf5d91218569a1129181d
CODE WITH JOSE DAVID <48775594+JoseCode-CO@users.noreply.github.com>
2024-08-07 20:39:46 -0500

    Models

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
+///Archivo maestro donde estan las configuraciones principales
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container.s
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
